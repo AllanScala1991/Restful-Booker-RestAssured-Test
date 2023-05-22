f3dc83ca1e0d8896b68736ea200d34410f3fae4d
Allan Scala <allanscala@gmail.com>
2023-05-22 17:31:11 -0300

    create get all bookings test

--- src/main/java/org/restassured/brooker/helpers/GetAllBookings.java
+++ src/main/java/org/restassured/brooker/helpers/GetAllBookings.java
@@ -1,31 +1,32 @@
-package org.restassured.brooker.scenarios;
+package org.restassured.brooker.helpers;
 
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
-import org.restassured.brooker.helpers.Config;
-import static org.hamcrest.Matchers.*;
+
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class CreateToken {
+public class GetAllBookings {
     private static final Map<String, LocalDate> bookingsDates = new HashMap<>();
     private static final Map<String, Object> payload = new HashMap<>();
 
     @BeforeClass
     public static void setupTest() {
         baseURI = new Config().getBaseUrl();
+
         bookingsDates.put("checkin", LocalDate.now());
         bookingsDates.put("checkout", LocalDate.now());
 
-        payload.put("firstname", "James");
-        payload.put("lastname", "Rodrigues");
+        payload.put("firstname", "Test");
+        payload.put("lastname", "Filter");
         payload.put("totalprice", 300);
         payload.put("depositpaid", true);
         payload.put("bookingdates", bookingsDates);
@@ -33,31 +34,31 @@ public class CreateToken {
     }
 
     @Test
-    public void t01_createNewBookingSuccessfully() {
+    public void t01_getAllBookings() {
         given()
+                .contentType("application/json")
+                .when()
+                .get("/booking")
+                .then()
+                .body("size()", is(greaterThan(0)))
+                .body("[0].bookingid", is(notNullValue()));
+    }
+
+    @Test
+    public void t02_filterBookingByName() {
+        String bookingId = given()
                 .contentType("application/json")
                 .body(payload)
                 .when()
                 .post("/booking")
                 .then()
-                .statusCode(200)
-                .body("bookingid", is(notNullValue()))
-                .body("booking.firstname", is("James"));
-    }
-
-    @Test
-    public void t02_tryCreateBookingWithInvalidPayloadKeys() {
-        Map<String, Object> invalidPayloadKeys = payload;
-        invalidPayloadKeys.remove("firstname");
-        invalidPayloadKeys.replace("lastname", "");
+                .extract().path("bookingid").toString();
 
         given()
                 .contentType("application/json")
-                .body(invalidPayloadKeys)
                 .when()
-                .post("/booking")
+                .get("/booking?firstname=Test&lastname=Filter")
                 .then()
-                .statusCode(500)
-                .statusLine("HTTP/1.1 500 Internal Server Error");
+                .body("bookingid", hasItem(Integer.parseInt(bookingId)));
     }
 }
